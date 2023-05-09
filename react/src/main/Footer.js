029f26cda17864773c9b4ba4ee369d7b35d588a2
Suraj Bade <115887200+srj121@users.noreply.github.com>
2023-05-10 01:46:21 +0530

    Add files via upload

--- react/src/main/Footer.js
+++ react/src/main/Footer.js
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
 
     return(
         <div className="footer-section">
-              <h1>fotter</h1>
+            <ul className="list-section-footer">
+          <li className="sample-list"><Link to={"/"}>sample</Link></li>
+          <li className="sample-list"><Link to={"/"}>sample</Link></li>
+          <li className="sample-list"><Link to={"/"}>sample</Link></li>
+          <li className="sample-list"><Link to={"/"}>sample</Link></li>
+          <li className="sample-list"><Link to={"/"}>sample</Link></li>
+        </ul>
+      <section class="mb-5">
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-8">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              distinctio earum repellat quaerat voluptatibus placeat nam,
+              commodi optio pariatur est quia magnam eum harum corrupti
+              dicta, aliquam sequi voluptate quas.
+            </p>
+          </div>
+        </div>
+      </section>
         </div>
         
     );
--- react/src/main/Header.js
+++ react/src/main/Header.js
@@ -2,13 +2,22 @@ import { Link } from "react-router-dom";
 
 function Header() {
 
+    function handleSearch() {
+
+
+    }
+
     return(
         <div className="header-section">
             <ul className="list-section-header">
-          <li>signup</li>
-          <li>login</li>
-          <li>search</li>
-          <li><Link to={"/about"}>About</Link></li>
+          <li className="signup-list"><Link to={"/signup"}>signup</Link></li>
+          <li className="login-list"><Link to={"/"}>login</Link></li>
+          <li className="search-box-list">search
+            <input type="text" name="search" 
+            onClick={handleSearch}>
+                </input>
+          </li>
+          <li className="about-list"><Link to={"/about"}>About</Link></li>
         </ul>
         </div>
         
