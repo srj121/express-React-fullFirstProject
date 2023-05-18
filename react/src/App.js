fef978c5c59f86c8aa700e9046cb7ab3690fa35e
Suraj Bade <115887200+srj121@users.noreply.github.com>
2023-05-18 20:38:28 +0530

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -1,15 +1,17 @@
-import Crudoperation from "./crud_operation/Crudoperation";
-import LoginPage from "./crud_operation/LoginPage";
+// import Crudoperation from "./crud_operation/Crudoperation";
+/*import LoginPage from "./crud_operation/LoginPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./crud_operation/SignUp";
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import Main from "./main/Main";
-import About from "./review/About";
+import About from "./review/About";*/
 
-function App() {
+
+export default function App() {
   return (
-    <BrowserRouter>
+    <h1>hi there!</h1>
+    /*<BrowserRouter>
     <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/login" element={<LoginPage />} />
@@ -18,9 +20,8 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
       <ToastContainer />
-    </BrowserRouter>
+    </BrowserRouter>*/
     
   );
 }
 
-export default App;
