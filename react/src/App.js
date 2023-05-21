121fb9fc60262d73645c6f990a10a496e51dc5a3
Suraj Bade <115887200+srj121@users.noreply.github.com>
2023-05-22 01:21:09 +0530

    from home

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,13 @@
 import Crudoperation from "./crud_operation/Crudoperation";
-import LoginPage from "./crud_operation/LoginPage";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import SignUp from "./crud_operation/SignUp";
+import LoginPage from "./login_Signup/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./login_Signup/SignUp";
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import Main from "./main/Main";
 import About from "./review/About";
 import {Test} from "./review/Test";
+import OtpVerification from "./login_Signup/OtpVerification";
 
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
     <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Signup" element={<SignUp />} />
+        <Route path="signup/otp" element={<OtpVerification />} />
         <Route path="/crudoperation" element={<Crudoperation />} />
         <Route path="/about" element={<About />} />
         <Route path="/test" element={<Test />} />
